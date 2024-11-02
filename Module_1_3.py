ad05e644695b324cea538a8d382ad2cffa708311
P1t1e <rkankulov01@gmail.com>
2024-11-02 11:17:19 +0300

    Add files via upload

---
+++ Module_1_3.py
@@ -0,0 +1,9 @@
+name = 'Руслан'
+print(name, type(name))
+age = 22
+print(age, type(age))
+new_age = age + 5
+print(new_age, type(new_age))
+name = 'is_student'
+print(name,bool(name))
+print(name, type(name))
